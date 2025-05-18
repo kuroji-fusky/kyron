2cb00624b1e931f7a7cfbbb690227d39dabc6aee
Kuro <hello@kurojifusky.com>
2025-05-18 17:49:07 +0800

    add https schema

--- archiver/utils/requests.py
+++ archiver/utils/requests.py
@@ -14,16 +14,18 @@ HEADERS = {
     "User-Agent": "For an archival project"
 }
 
+HTTPS_SCHEMA = "https://"
+
 
 def get_call(url: str, *, params: Optional[dict] = None):
-    return rs.get(url, params=params, headers=HEADERS)
+    return rs.get(f"{HTTPS_SCHEMA}{url}", params=params, headers=HEADERS)
 
 
 def post_call(url, params: Optional[dict], json_payload: dict[str, Any]):
-    return rs.post(url, params=params, json=json.dumps(json_payload), headers=HEADERS)
+    return rs.post(f"{HTTPS_SCHEMA}{url}", params=params, json=json.dumps(json_payload), headers=HEADERS)
 
 
-def request_soup(url: str, params: dict):
+def get_soup(url: str, *, params: Optional[dict] = None):
     _req = get_call(url, params=params)
 
     return BeautifulSoup(_req.text, "html.parser")
