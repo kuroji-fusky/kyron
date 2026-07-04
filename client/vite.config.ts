ad9d91e3fe725fe416760cfaabfd129446252148
Kuro <hello@kurojifusky.com>
2026-07-04 09:03:23 +0800

    added web dashboard + compose file, refactor configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,23 @@
+import { defineConfig } from "vite"
 import tailwindcss from "@tailwindcss/vite"
 import { sveltekit } from "@sveltejs/kit/vite"
-import { defineConfig } from "vite"
+import adapter from "@sveltejs/adapter-static"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [
+    tailwindcss(),
+    sveltekit({
+      preprocess: vitePreprocess(),
+      adapter: adapter({
+        pages: ".vite/renderer/main_window",
+      }),
+      compilerOptions: {
+        // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+        runes: ({ filename }) => filename.split(/[/\\]/).includes("node_modules") ? undefined : true
+      },
+    })
+  ],
   optimizeDeps: {
     exclude: ["@lucide/svelte"],
   },
